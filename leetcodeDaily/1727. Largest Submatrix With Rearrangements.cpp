01799af3488b590eb1dea8e3ff8fc3bb0e88fb60
Surbhi Singla <singlasurbhi104@gmail.com>
2026-03-17 21:07:35 +0530

    1727

---
+++ leetcodeDaily/1727. Largest Submatrix With Rearrangements.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int largestSubmatrix(vector<vector<int>>& matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        int maxArea = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] != 0 && i > 0) {
+                    matrix[i][j] += matrix[i - 1][j];
+                }
+            }
+            vector<int> currentRow = matrix[i];
+            sort(currentRow.begin(), currentRow.end(), greater<int>());
+            for (int k = 0; k < n; k++) {
+                maxArea = max(maxArea, currentRow[k] * (k + 1));
+            }
+        }
+        return maxArea;
+    }
+};
