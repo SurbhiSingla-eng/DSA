d64ebcdc381fff446c6a792381f4b6d58a406193
Surbhi Singla <singlasurbhi104@gmail.com>
2026-03-27 22:39:43 +0530

    2946

---
+++ leetcodeDaily/2946. Matrix Similarity After Cyclic Shifts.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool areSimilar(vector<vector<int>>& mat, int k) {
+        int m = mat.size();
+        int n = mat[0].size();
+        int shift = k % n;
+        if (shift == 0) return true;
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (mat[i][j] != mat[i][(j + shift) % n]) {
+                    return false;
+                }
+            }
+        }   
+        return true;
+    }
+};
