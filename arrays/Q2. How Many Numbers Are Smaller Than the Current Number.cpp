4d69b8e15782dba06e00a1280b069236a96abb17
Surbhi Singla <singlasurbhi104@gmail.com>
2026-04-09 22:21:05 +0530

    quest

---
+++ arrays/Q2. How Many Numbers Are Smaller Than the Current Number.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> smallerNumbersThanCurrent(vector<int>& nums) {
+        vector<int> arr;
+        for (int i = 0; i < nums.size(); i++){
+            // int small = nums[i];
+            int count = 0;
+            for (int j = 0; j < nums.size(); j++){
+                if (j != i && nums[j] < nums[i]){
+                    count++;
+                }
+            }
+            arr.push_back(count);
+        }
+        return arr;
+    }
+};
